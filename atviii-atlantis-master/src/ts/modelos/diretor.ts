a949d7032772b790d3899bd986f50ec3a762c433
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-09 14:17:57 -0300

    atualizei o diretor

--- atviii-atlantis-master/src/ts/modelos/diretor.ts
+++ atviii-atlantis-master/src/ts/modelos/diretor.ts
@@ -1,4 +1,10 @@
 import { Acomodacao } from "./acomodacao";
+import CasalSimples from "./casalSimples";
+import FamiliaSimples from "./FamiliaSimples";
+import FamiliaSuper from "./FamiliaSuper";
+import FamiliaPlus from "./FamiliaPlus";
+import SolteiroSimples from "./solteiroSimples";
+import SolteiroMais from "./solteiroMais";
 
 export default class Diretor {
   private acomodacoes: Acomodacao[];
@@ -14,19 +20,23 @@ export default class Diretor {
 
   // Lista todas as acomodações
   listarAcomodacoes(): void {
-    this.acomodacoes.forEach(acomodacao => {
+    this.acomodacoes.forEach((acomodacao) => {
       console.log(acomodacao);
     });
   }
 
   // Encontra uma acomodação pelo nome
   encontrarAcomodacaoPorNome(nome: string): Acomodacao | undefined {
-    return this.acomodacoes.find(acomodacao => acomodacao.nome === nome);
+    return this.acomodacoes.find(
+      (acomodacao) => acomodacao.nome === nome
+    );
   }
 
   // Remove uma acomodação pelo nome
   removerAcomodacao(nome: string): void {
-    const index = this.acomodacoes.findIndex(acomodacao => acomodacao.nome === nome);
+    const index = this.acomodacoes.findIndex(
+      (acomodacao) => acomodacao.nome === nome
+    );
     if (index !== -1) {
       this.acomodacoes.splice(index, 1);
     } else {
@@ -36,6 +46,45 @@ export default class Diretor {
 
   // Método para contar quantas acomodações de um tipo específico existem
   contarPorTipo(tipo: string): number {
-    return this.acomodacoes.filter(acomodacao => acomodacao.nome === tipo).length;
+    return this.acomodacoes.filter(
+      (acomodacao) => acomodacao.nome === tipo
+    ).length;
+  }
+
+  // Métodos para criar e adicionar diferentes tipos de acomodações
+  criarCasalSimples(): Acomodacao {
+    const acomodacao = new CasalSimples();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
+  }
+
+  criarFamiliaSimples(): Acomodacao {
+    const acomodacao = new FamiliaSimples();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
+  }
+
+  criarFamiliaSuper(): Acomodacao {
+    const acomodacao = new FamiliaSuper();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
+  }
+
+  criarFamiliaPlus(): Acomodacao {
+    const acomodacao = new FamiliaPlus();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
+  }
+
+  criarSolteiroSimples(): Acomodacao {
+    const acomodacao = new SolteiroSimples();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
+  }
+
+  criarSolteiroMais(): Acomodacao {
+    const acomodacao = new SolteiroMais();
+    this.adicionarAcomodacao(acomodacao);
+    return acomodacao;
   }
 }
